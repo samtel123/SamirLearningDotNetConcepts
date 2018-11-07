0246dfcbb9a9f9a8c499cc98357d0ddbe0d3b969
Samir Damani <samtel123@gmail.com>
2018-11-07 21:52:52 +0530

    sync to master branch on 16:38

---
+++ StudyExamples/StudyExamples/TaskParallalIntro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyExamples
+{
+    class TaskParallalIntro
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 10, x =>
+                {
+                    Console.WriteLine("value of i = {0}", x);
+                });
+            Console.ReadKey();
+
+            //sample code for action delegate with single argument with no return type. passing entire function pointer to method
+            GetData(0, 20, x => { Console.Write("hello world"); Console.WriteLine("value of x = {0}", x); });
+            Console.ReadKey();
+        }
+
+        //passing single argument delegate to this function
+        static void GetData(int min, int max, Action<int> mydelefun)
+        {
+            for (int i = min; i < max; i++)
+            {
+                mydelefun(i);
+            }
+        }
+
+    }
+}
